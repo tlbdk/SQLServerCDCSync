86de49ee13264db8caf79fb2288d47739b9196e9
Troels Liebe Bentsen <tlb@nversion.dk>
2014-08-19 15:27:35 +0200

    Finish Merge Load package generator and minor fixes

--- SQLServerCDCSync/SQLServerCDCSync.cs
+++ SQLServerCDCSync/SQLServerCDCSync.cs
@@ -9,6 +9,7 @@ using Microsoft.SqlServer.Dts.Pipeline.Wrapper;
 using System.Globalization;
 using System.Data.SqlClient;
 using Attunity.SqlServer.CDCControlTask;
+using Microsoft.SqlServer.Dts.Tasks.ExecuteSQLTask;
 
 // Links : EzAPI
 // http://social.msdn.microsoft.com/Forums/sqlserver/en-US/eb8b10bc-963c-4d36-8ea2-6c3ebbc20411/copying-600-tables-in-ssis-how-to?forum=sqlintegrationservices
@@ -25,7 +26,12 @@ namespace SQLServerCDCSync
             Application app = new Application();
             Package package = new Package();
 
+            package.Name = "CDC Merge Load Package for tables " + String.Join(", ", tables);
+
             // Add connection managers
+            ConnectionManager sourceManager = package.Connections.Add(string.Format("ADO.NET:{0}", typeof(SqlConnection).AssemblyQualifiedName));
+            sourceManager.ConnectionString = sourceconn;
+            sourceManager.Name = "Source Connection";
             ConnectionManager destinationManager = package.Connections.Add(string.Format("ADO.NET:{0}", typeof(SqlConnection).AssemblyQualifiedName));
             destinationManager.ConnectionString = destinationconn;
             destinationManager.Name = "Destination Connection";
@@ -37,19 +43,27 @@ namespace SQLServerCDCSync
 
             foreach (var table in tables)
             {
-                // Get Colums from database
-                SqlCommand sqlcmd = new System.Data.SqlClient.SqlCommand("SELECT name FROM sys.columns WHERE object_id = OBJECT_ID('"+ table +"')", sourceConnnection);
+                var identityColums = new HashSet<String>();
                 var colums = new List<string>();
+                // Get Colums from database
+                SqlCommand sqlcmd = new System.Data.SqlClient.SqlCommand("SELECT name, is_identity FROM sys.columns WHERE object_id = OBJECT_ID('" + table + "')", sourceConnnection);
                 using (SqlDataReader sqlRd = sqlcmd.ExecuteReader())
                 {
                     while (sqlRd.Read())
                     {
                         colums.Add(sqlRd.GetString(0));
+                        if (sqlRd.GetBoolean(1))
+                        {
+                            identityColums.Add(sqlRd.GetString(0));
+                        }
                     }
                 }
 
                 // Create the merge SQL statement
                 var merge_sql =
+                    "declare @start_lsn binary(10);\n" +
+                    "declare @end_lsn binary(10);\n" + 
+                    "declare @rowcount int;\n" +
                     "set @rowcount = 0;\n" +
                     "set @start_lsn = sys.fn_cdc_increment_lsn(CONVERT(binary(10), SUBSTRING(@cdcstate, CHARINDEX('/CS/', @cdcstate) + 4, CHARINDEX('/', @cdcstate, CHARINDEX('/CS/', @cdcstate) + 4) - CHARINDEX('/CS/', @cdcstate) - 4), 1));\n" +
                     "set @end_lsn = convert(binary(10), SUBSTRING(@cdcstate, CHARINDEX('/CE/', @cdcstate) + 4, CHARINDEX('/', @cdcstate, CHARINDEX('/CE/', @cdcstate) + 4) - CHARINDEX('/CE/', @cdcstate) - 4), 1);\n" +
@@ -57,13 +71,13 @@ namespace SQLServerCDCSync
                         "SET IDENTITY_INSERT [dbo].[" + table + "] ON;\n" +
                         "MERGE [dbo].[" + table + "] AS D\n" +
                         "USING " + sourcedbname + ".cdc.fn_cdc_get_net_changes_Test1(@start_lsn, @end_lsn, 'all with merge') AS S\n" +
-                        "ON (D.Id = S.Id)\n" +
+                        "ON (" + String.Join(" AND ", identityColums.Select(s => "D." + s + " = " + "S." + s)) + ")\n" +
                         // Insert
                         "WHEN NOT MATCHED BY TARGET AND __$operation = 5\n" +
                             "THEN INSERT (" + String.Join(", ", colums) + ") VALUES(" + String.Join(", ", colums.Select(s => "S." + s)) + ")\n" +
                         // Update
                         "WHEN MATCHED AND __$operation = 5\n" +
-                            "THEN UPDATE SET " + String.Join(", ", colums.Select(s => "D." + s + " = S." + s)) + "\n" +
+                            "THEN UPDATE SET " + String.Join(", ", colums.Where(s => !identityColums.Contains(s)).Select(s => "D." + s + " = S." + s)) + "\n" +
                         // Delete
                         "WHEN MATCHED AND __$operation = 1\n" +
                             "THEN DELETE\n" +
@@ -71,7 +85,61 @@ namespace SQLServerCDCSync
                         "set @rowcount = @@ROWCOUNT;\n" +
                         "SET IDENTITY_INSERT [dbo].[" + table + "] OFF;\n" +
                     "END\n" +
-                    "SELECT @rowcount as NumberOfRecords;\n";
+                    "SELECT @rowcount as RowsUpdated;\n";
+
+                // Add variables
+                package.Variables.Add("CDC_State_" + table, false, "User", "");
+                package.Variables.Add("RowsUpdated_" + table, false, "User", "");
+
+                // Add CDC Get CDC Processing Range
+                TaskHost cdcControlTaskGetRange = package.Executables.Add("Attunity.CdcControlTask") as TaskHost;
+                cdcControlTaskGetRange.Name = "Get CDC Processing Range for " + table;
+                cdcControlTaskGetRange.Properties["Connection"].SetValue(cdcControlTaskGetRange, sourceManager.ID);
+                cdcControlTaskGetRange.Properties["TaskOperation"].SetValue(cdcControlTaskGetRange, CdcControlTaskOperation.GetProcessingRange);
+                cdcControlTaskGetRange.Properties["StateConnection"].SetValue(cdcControlTaskGetRange, destinationManager.ID);
+                cdcControlTaskGetRange.Properties["StateVariable"].SetValue(cdcControlTaskGetRange, "User::CDC_State_" + table);
+                cdcControlTaskGetRange.Properties["AutomaticStatePersistence"].SetValue(cdcControlTaskGetRange, true);
+                cdcControlTaskGetRange.Properties["StateName"].SetValue(cdcControlTaskGetRange, "CDC_State_" + table);
+                cdcControlTaskGetRange.Properties["StateTable"].SetValue(cdcControlTaskGetRange, "[dbo].[cdc_states]");
+                cdcControlTaskGetRange.DelayValidation = true;
+
+                // Add Execute Merge Command
+                TaskHost executeMergeSQL = package.Executables.Add("STOCK:SQLTask") as TaskHost;
+                executeMergeSQL.Name = "Execute Merge Command for " + table;
+                executeMergeSQL.Properties["Connection"].SetValue(executeMergeSQL, destinationManager.ID);
+                executeMergeSQL.Properties["SqlStatementSource"].SetValue(executeMergeSQL, merge_sql);
+                var executeMergeSQLTask = executeMergeSQL.InnerObject as ExecuteSQLTask;
+                executeMergeSQLTask.ResultSetType = ResultSetType.ResultSetType_SingleRow;
+                // Add input parameter binding
+                executeMergeSQLTask.ParameterBindings.Add();
+                IDTSParameterBinding parameterBinding = executeMergeSQLTask.ParameterBindings.GetBinding(0);
+                parameterBinding.DtsVariableName = "CDC_State_" + table;
+                parameterBinding.ParameterDirection = ParameterDirections.Input;
+                parameterBinding.DataType = 16;
+                parameterBinding.ParameterName = "@cdcstate";
+                parameterBinding.ParameterSize = -1;
+                // Add result set binding, map the id column to variable
+                executeMergeSQLTask.ResultSetBindings.Add();
+                IDTSResultBinding resultBinding = executeMergeSQLTask.ResultSetBindings.GetBinding(0);
+                resultBinding.ResultName = "0";
+                resultBinding.DtsVariableName = "User::RowsUpdated_" + table;
+
+
+                // Add Mark CDC Processed Range
+                TaskHost cdcControlTaskMarkRange = package.Executables.Add("Attunity.CdcControlTask") as TaskHost;
+                cdcControlTaskMarkRange.Name = "Mark CDC Processed Range for " + table;
+                cdcControlTaskMarkRange.Properties["Connection"].SetValue(cdcControlTaskMarkRange, sourceManager.ID);
+                cdcControlTaskMarkRange.Properties["TaskOperation"].SetValue(cdcControlTaskMarkRange, CdcControlTaskOperation.MarkInitialLoadEnd);
+                cdcControlTaskMarkRange.Properties["StateConnection"].SetValue(cdcControlTaskMarkRange, destinationManager.ID);
+                cdcControlTaskMarkRange.Properties["StateVariable"].SetValue(cdcControlTaskMarkRange, "User::CDC_State_" + table);
+                cdcControlTaskMarkRange.Properties["AutomaticStatePersistence"].SetValue(cdcControlTaskMarkRange, true);
+                cdcControlTaskMarkRange.Properties["StateName"].SetValue(cdcControlTaskMarkRange, "CDC_State_" + table);
+                cdcControlTaskMarkRange.Properties["StateTable"].SetValue(cdcControlTaskMarkRange, "[dbo].[cdc_states]");
+                cdcControlTaskMarkRange.DelayValidation = true;
+
+                // Configure precedence
+                (package.PrecedenceConstraints.Add((Executable)cdcControlTaskGetRange, (Executable)executeMergeSQL)).Value = DTSExecResult.Success;
+                (package.PrecedenceConstraints.Add((Executable)executeMergeSQL, (Executable)cdcControlTaskMarkRange)).Value = DTSExecResult.Success;
             }
 
             app.SaveToXml(filename, package, null);
@@ -81,6 +149,8 @@ namespace SQLServerCDCSync
         {
             Application app = new Application();
             Package package = new Package();
+
+            package.Name = "CDC Initial Load Package for tables " + String.Join(", ", tables);
 
             // Add connection managers
             ConnectionManager sourceManager = package.Connections.Add(string.Format("ADO.NET:{0}", typeof(SqlConnection).AssemblyQualifiedName));
